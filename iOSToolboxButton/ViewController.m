572b07fa961df086fc2b9746d9a179a5e3bf8855
iOS Toolbox Team <team@iostoolbox.com>
2014-03-24 18:10:01 +0100

    Button sample

--- iOSToolboxButton/ViewController.m
+++ iOSToolboxButton/ViewController.m
@@ -17,13 +17,23 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    self.view.backgroundColor = [UIColor colorWithRed:0.224 green:0.263 blue:0.369 alpha:1];
+    
+    // buy button
+    UIButton *buyButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [buyButton setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [buyButton setBackgroundImage:[UIImage imageNamed:@"buyButton"] forState:UIControlStateNormal];
+    [buyButton setTitleColor:[UIColor colorWithRed:1 green:1 blue:1 alpha:1] forState:UIControlStateNormal];
+    [buyButton setTitle:NSLocalizedString(@"Buy Plugin Today!", nil) forState:UIControlStateNormal];
+    buyButton.titleLabel.font = [UIFont fontWithName:@"HelveticaNeue-Bold" size:23];
+    [self.view addSubview:buyButton];
+    
+    // center buyButton horizontally in superview
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:buyButton attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0.0]];
+    
+    // align buyButton from the top
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-81.5-[buyButton]" options:0 metrics:nil views:NSDictionaryOfVariableBindings(buyButton)]];
 }
 
 @end
